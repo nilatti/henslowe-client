4fea3cc03056c1cdf327232fa1b1d8d47ec6182c
Aili Huber <alisha.huber@gmail.com>
2020-05-19 07:47:48 -0400

    updated url in api

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: `http://localhost:3001/api/`,
+  baseURL: `https://henslowescloud.com:3001/api/`,
   headers: {"Authorization" : `${window.localStorage.getItem('token')}`}
 });
