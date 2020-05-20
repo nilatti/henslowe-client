808d4c8ffb40b0e5720de76e0ffae55c76fc9f53
Aili Huber <alisha.huber@gmail.com>
2020-05-20 16:57:12 -0400

    trying again to make login work

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: `https://henslowescloud.com:3001/api/`,
-  baseURL: 'http://localhost:3001/api',
+  baseURL: `https://henslowescloud.com:3001/api/`,
+  // baseURL: 'http://localhost:3001/api',
   headers: {"Authorization" : `${window.localStorage.getItem('token')}`}
 });
