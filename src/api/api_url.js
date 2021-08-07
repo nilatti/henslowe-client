1568ba9ba7f973f578ab5e56d1bf5f19769ff7fe
Aili Huber <alisha.huber@gmail.com>
2021-08-07 18:42:20 -0400

    removed all references to specific api

--- src/api/api_url.js
+++ src/api/api_url.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 export const baseUrl = "http://google.com";
-// "http://hcapi-env.eba-epmrxskb.us-east-1.elasticbeanstalk.com/api";
-
 export default axios.create({
   baseURL: baseUrl,
   headers: {
