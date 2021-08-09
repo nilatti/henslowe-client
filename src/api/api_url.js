eef1378f6d878554b797dd01f1a5d1de37b7691f
Aili Huber <alisha.huber@gmail.com>
2021-08-09 13:33:27 -0400

    updated url to be ssl api.henslowe.com

--- src/api/api_url.js
+++ src/api/api_url.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-export const baseUrl =
-  "http://hcapi-env.eba-epmrxskb.us-east-1.elasticbeanstalk.com/api";
+export const baseUrl = "https://api.henslowe.com/api";
 export default axios.create({
   baseURL: baseUrl,
   headers: {
