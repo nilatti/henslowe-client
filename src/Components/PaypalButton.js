8a4424c10a19fee697c7953888f93511ad459769
Aili Huber <alisha.huber@gmail.com>
2021-08-13 09:27:29 -0400

    updated total

--- src/Components/PaypalButton.js
+++ src/Components/PaypalButton.js
@@ -7,15 +7,15 @@ export default function PaypalButton() {
     <PaypalContainer>
       <div>
         <em>
-          <strong>This service will become unavailable on September 3.</strong>
+          <strong>This service will become unavailable on September 17.</strong>
           <br />
           It costs $100/month to operate.
           <br />
           If I get $100 in donations, I'll leave it up for another month.
           <br />
           <strong>
-            Update: Four awesome people have donated a total of $50, which
-            allowed us to extend our shut off date by two weeks!
+            Update: Six awesome people have donated a total of $100, which
+            allowed us to extend our shut off date by a month!
           </strong>
           <br />
           Please help pay for server costs by hitting the donate button!
