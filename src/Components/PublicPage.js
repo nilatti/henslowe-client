6b44ee7d53940d7513bae1857a562c24664e02a3
Aili Huber <alisha.huber@gmail.com>
2021-09-21 12:40:49 -0400

    temp shutdown

--- src/Components/PublicPage.js
+++ src/Components/PublicPage.js
@@ -19,9 +19,21 @@ const PublicPage = () => {
   return (
     <PublicContainer>
       <h2>Henslowe's Cloud</h2>
-      <Navigation />
+      {/* <Navigation />
       <Main />
-      <Footer />
+      <Footer /> */}
+      <div class="temp">
+        Text tools are temporarily offline until the launch of our book,{" "}
+        <a href="http://www.cuttingplays.com">Cutting Plays for Performance.</a>{" "}
+        <a
+          class="nav-link"
+          href="https://7eb01415.sibforms.com/serve/MUIEANSIBpHX1ebcwI6sCz6rKT9E1gYsq_cQlSr3TuGbDsV6aAx9lsp3YKU_mGdYu5viKpe-80RPz4vZb7py3hn5xhN8g-DozWbOvHR4GHv0_Ua_qYVpyKUKEytxSl_be1APkglNZ0ctaK5baVWx7Pvu5tw0DzrT-sLpBDt7cA00oiopekJzDTkyuxVEvLQP5pT-6Wo9j4V_ZTSq"
+          target="_blank"
+        >
+          Subscribe to our newsletter
+        </a>{" "}
+        for updates!
+      </div>
     </PublicContainer>
   );
 };
